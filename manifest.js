8d215674a0ad9a072f18ff81fd8337d5376682d5
khalcyon <174127342+khalcyon@users.noreply.github.com>
2026-04-03 17:57:57 -0500

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,12 @@
+{
+    "id": "clientsideblock",
+    "name": "ClientSideBlock",
+    "description": "Locally hide messages, DMs, and profile entries from any user. No server-side block — the other person never knows.",
+    "authors": [
+        {
+            "name": "Samwich",
+            "id": "534198797985964032"
+        }
+    ],
+    "main": "index.js"
+}
